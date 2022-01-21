7c2300d333a6d6c2a96109bac3786f7c34a00a6f
Brian <twene521@gmail.com>
2022-01-21 18:04:35 +0000

    fixed error with one of the routes

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use("/api", api);
 app.use("/download", download);
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
