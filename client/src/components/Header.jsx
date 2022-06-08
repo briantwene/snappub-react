a247009c0cdfb9816fd29e009b98f800166774c4
Brian Twene <brian.twene@optiim.com>
2022-06-08 17:50:24 +0100

    added a dropdown menu for selecting different subreddits

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,44 @@
 // #Filename: Header.jsx
 // #Author: Brian Twene (@bt521)
 // #Date:12/12/21
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 import * as RiIcons from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="header">
       <div className="logo">
         <span className="snap-logo">Snap-Pub</span>
       </div>
-      {/* <div className="logo-buttons">
-        <Link to="/download" className="btn navlink download-app">
-          <RiIcons.RiDownload2Fill />
-          <span>Download</span>
-        </Link>
-        <Link to="/settings" className="btn navlink settings">
-          <RiIcons.RiSettings5Fill />
-          <span>Settings</span>
-        </Link>
-      </div> */}
+      <div
+        className="dropdown"
+        onClick={() => setIsOpen((prevState) => !prevState)}
+      >
+        <button className="current-subreddit">
+          <img
+            className="current-subreddit-image"
+            src="https://a.thumbs.redditmedia.com/APweUko3qLJ0prsQI1giluMwBdcVnokw9_yZcby4SB8.png"
+            alt=""
+          />
+          <span>w/wallpaper</span>
+          <span className="dropdown-icon">
+            <RiIcons.RiArrowDropDownFill />
+          </span>
+        </button>
+
+        <ul className={`dropdown-content${isOpen ? '__show' : ''}`}>
+          <li>wallpaper</li>
+          <li>wallpaper</li>
+          <li>wallpaper</li>
+          <li>wallpaper</li>
+          <li>wallpaper</li>
+        </ul>
+      </div>
     </div>
   );
 }
